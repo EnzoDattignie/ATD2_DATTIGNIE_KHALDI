87702c9bd2488936cb1aa706ba40c4126de3e546
Enzo <enzo.dattignie@gmail.com>
2025-03-25 15:36:17 +0100

    test

--- test.py
+++ test.py
@@ -9,3 +9,21 @@ string = "Autoset"
 oscillo.write(string)
 string = "Meas:"+"High?"
 print(oscillo.query(string))
+
+def bestTimeScale (liste,t,max_index) :
+    max = liste[max_index]
+    res =  False
+    i = max_index
+    compteur == 0
+    while (i < len(liste)) and (compteur != 10):
+        if liste[i] < max/100 :
+            compteur += 1
+        else :
+            compteur = 0
+        i += 1
+    if compteur == 10 :
+        res = (t[i]-t[max_index])*1.1/10
+    return res
+        
+
+
