e1f0fe207d48795a67bc44ceb5fc9b136d931c91
EnzoDattignie <enzo.dattignie@gmail.com>
2025-03-30 18:16:31 +0200

    prototype final

--- oscillo.py
+++ oscillo.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Le but de ce pprogramme est de tester si on peut conttroller l'oscillo en manipulant des string
+
 #pip install pyvisa-py
 
 import pyvisa
@@ -7,59 +10,198 @@ import numpy as np
 import sys
 
 
-#Initialisation du programme
-rm = pyvisa.ResourceManager()
-oscillo = rm.open_resource('ASRL3::INSTR')  #port serie 3
-oscillo.write(":Autos") #Autoset
-oscillo.write(':ACQ:RECO 1e+5') #Change le nombre de valeurs a 10000
-
-#On récupere la data sur les 2 channels
-vert_scale1,time_scale1,waveform1,high1 = lecture(1)
-vert_scale2,time_scale2,waveform2,high2 = lecture(2)
-
-#On les met en valeurs d'intensite
-waveform1 = norm1(waveform1,high1)
-waveform2 = norm2(waveform2,high2)
-
-    
-#On affiche le graph
-x = np.linspace(-5*time_scale1,5*time_scale1,len(val))
-plt.plot(x, waveform1, label="CH1")
-plt.plot(x,waveform2, label="CH2")
-plt.grid()
-plt.xlabel("Time (s)")
-plt.ylabel("Signal (V)")
-plt.legend()
-plt.show()
-
 #Création des différentes fonctions utiles
+
+def question(_period,_volt, _duty) :
+    answer = input("Que voulez vous modifier ? (volt,period,duty)")
+    val = ""
+    if answer == "volt" :
+        while type(val) != type(0.5) :
+            val = input("Donner la nouvelle valeur de potentiel : ")
+            try :
+                val = float(val)
+                _volt = val
+            except :
+                print ("erreur {} pas un float".format(val))
+    elif answer == "period" :
+        while type(val) != type(0.5) :
+            val = input("Donner la nouvelle valeur de la periode : ")
+            try :
+                val = float(val)
+                _period = val
+            except :
+                print ("erreur {} pas un float".format(val))
+    elif answer == "duty" :
+        while type(val) != type(0.5) :
+            val = input("Donner la nouvelle valeur du duty entre 0 et 100 (%) : ")
+            try :
+                val = float(val)
+                if val >= 0 and val <= 100 :
+                    _duty = val
+                else :
+                    print ("La valeur donnéee n'est pas entre 0 et 100, on conserve la valeur précédente")
+            except :
+                print ("erreur {} pas un float".format(val))
+    else : 
+        print ("La valeur donnée a modifier n'est pas correcte")
+    print ("Les valeurs rentrées sont : \nVolt = {}\nPeriode = {}\nDuty = {} %".format(_volt,_period,_duty))
+    answer = input("Voulez vous modifier une valeur ? (y/n)")
+    if answer == "y" :
+        _period,_volt,_duty = question(_volt,_period,_duty)
+    return _period,_volt,_duty
+
+
+def getGBF () :
+    _period = gbf.query(":SOURce:PERiod?")
+    _duty = gbf.query (":SOURce:DCYCle?")
+    _volt = gbf.query(":SOURce:VOLTage?")
+    vidage(gbf)
+    return _period, _volt, _duty
+
+
+def setGBF (period, voltage, duty) :
+    gbf.write(":SOURce:PERiod "+str(period))
+    gbf.write(":SOURce:VOLTage "+str(voltage))
+    if duty > 100 or duty < 0 :
+        print ("Duty values incorrect, set to 1%")
+        duty = 1
+    gbf.write(":SOURce:PULSe:DCYCle "+str(duty))
+    vidage(gbf)
+
 def lecture(CH,N=5):
-    oscillo.write(':MEAS:SOUR'+CH+' CH'+CH) #Vraiment pas sur de cette ligne
-    oscillo.write(':Header on')
-    time.sleep(0.5)
-    oscillo.write(':ACQ'+CH+":MEM?")
+    oscillo.write(':MEAS:SOUR'+str(CH)+' CH'+str(CH))
+    time.sleep(1)
+    oscillo.write(':ACQ'+str(CH)+":MEM?")
     _header = oscillo.read()
-    _vert_scale=float(header.split(";")[12].split(',')[1])
-    _time_scale=float(header.split(";")[15].split(',')[1])
-    _waveform = oscillo.read_binary_values(datatype='h') #Essayer is_big_endian=True
-    _high=float(oscillo.query(":Meas:HIGH?"))
-    return _vert_scale,_time_scale,_waveform,_high
+    try : #Parfois le header contient juste un \n (Pourquoi ?) donc on try, si ca marche ok, sinon on reread le header
+        _vert_scale=float(_header.split(";")[12].split(',')[1])
+        _time_scale=float(_header.split(";")[15].split(',')[1])
+    except:
+        _header = oscillo.read()
+        _vert_scale=float(_header.split(";")[12].split(',')[1])
+        _time_scale=float(_header.split(";")[15].split(',')[1])
+    _waveform = oscillo.read_binary_values(datatype='h',is_big_endian=True) #Essayer is_big_endian=True
+    return _vert_scale,_time_scale,_waveform
 
-def norm1(_waveform,_high) : #Ce qu'on utilise atm pour avoir les vraies valeurs de la waveform
-    wave = _waveform
-    for i in range(0,len(wave)) :
-        wave[i] = wave[i]/max(wave)*_high
-    return wave
 
-def norm2(_waveform,_vert_scale,AD=25) : #Ce qui est sensé marcher selon la docu
+
+def norm(_waveform,_vert_scale,AD=25) : #Ce qui est sensé marcher selon la docu
     wave = _waveform
     for i in range(0,len(wave)) :
         wave[i] = wave[i]/AD * _vert_scale
     return wave
 
 
-def vidage() :
-    for i in range(0,1000) :
-        oscillo.read_raw()
+def vidage(instr=oscillo) :
+    k = 0
+    while k == 0 :
+        try :
+            instr.read_raw()
+            vidage()
+        except :
+            k = 1
+
+def sauvegarde(fichier,t,liste,liste2)
+    with open(fichier,'w') as file :
+        for i in range (0, len(t)) :
+            file.writelines([str(t[i]),",",str(liste[i]),",",str(liste2[i])"\n"])
+
+def graph(_vert_scale1, _vert_scale2,_t1, _t2):
+    plt.plot(_t1, _waveform1,'orange', label="CH1",)
+    plt.plot(_t2,_waveform2,'c', label="CH2")
+    plt.grid()
+    plt.xlabel("Time (s)")
+    plt.ylabel("Signal (V)")
+    plt.legend()
+    plt.show()
+
+def notation_inge(nb) : #Fonction qui ressort n'importe quel nombre en notation inge lisible par l'oscillo
+    if nb > 0 :
+        exposant = int(np.floor(np.log10(nb)))
+        nb = round(nb*10**(-1*exposant),5)
+        nb = int(str(nb)[0])
+        if nb != 1 and nb != 2 and nb != 5 :
+            if nb > 5 :
+                nb = 1
+                exposant += 1
+            elif nb > 2 :
+                nb = 5
+            elif nb > 1 :
+                nb = 2
+        string = str(nb)+"e"+str(exposant)
+    else :
+        string = "0"
+    return string
+
+def max_index(waveform1): #Fonction retournant l'index de valeur maximal d'une liste
+    max_value = waveform1[0]
+    max_index = 0
+    for i in range(1, len(waveform1)):
+        if waveform1[i] > max_value:
+            max_value = waveform1[i]
+            max_index = i
+    return max_index
+
+############################################################################################
+        
+#Initialisation du programme
+rm = pyvisa.ResourceManager()
+oscillo = rm.open_resource('ASRL3::INSTR')  #port serie 3
+gbf = rm.open_resource('USB0::0x1AB1::0x0642::DG1ZA241701521::INSTR') #Remplacer par le bon port
+
+#On check que les deux fonctionnent
+print(oscillo.query("*IDN?"))
+print(gbf.query("*IDN?"))
+vidage()
+
+oscillo.write(':ACQ:RECO 1e+5') #Change le nombre de valeurs a 10000
+gbf.write(":SOURce:FUNCtion PULSE") #On établit la forme du signal comme un pulse (a voir si il faut changer avec SQUare)
+
+#On récupère toutes les données du gbf
+gbf_period, gbf_volt, gbf_duty = getGBF()
+gbf_period = float(gbf_period)
+gbf_volt = float(gbf_volt)
+gbf_duty = float(gbf_duty)
+
+#On demande a l'utilisateur de les changer
+print ("Les valeurs rentrées sont : \nVolt = {}\nPeriode = {}\nDuty = {} %".format(gbf_volt,gbf_period,gbf_duty))
+answer = input("Voulez vous modifier une valeur ? (y/n)")
+if answer == "y" :
+    gbf_period,gbf_volt,gbf_duty = question(gbf_period,gbf_volt,gbf_duty)
+
+#On change les valeurs du gbf et on change la timebase de l'oscillo
+setGBF(gbf_period,gbf_volt,gbf_duty)
+timescale_str = notation_inge(gbf_period/10)
+oscillo.write(":TIMebase:SCALe "+timescale_str)
+
+time_pos = float(oscillo.query(":TIMebase:POSition?"))
+#On fait une premiere mesure pour bien aligner le timepos pour 
+vert_scale1,time_scale1,waveform1 = lecture(1)
+t1 = np.linspace(-5*time_scale1,5*time_scale1,len(waveform1))
+vidage()
+
+time_pos = notation_inge(time_pos + t1[max_index(waveform1)]*0.9) #J'hésite avec le signe - pour que le pic soit au départ de notre graph
+oscillo.write(":TIMebase:POSition "+time_pos)
+
+#Vraie bonne mesure de notre oscillo
+vert_scale1,time_scale1,waveform1 = lecture(1)
+time.sleep(0.5)
+vert_scale2,time_scale2,waveform2 = lecture(2)
+waveform1 = norm(waveform1,vert_scale1)
+waveform2 = norm(waveform2,vert_scale2)
+t1 = np.linspace(-5*time_scale1,5*time_scale1,len(waveform1))
+t2 = np.linspace(-5*time_scale2,5*time_scale2,len(waveform2))
+
+graph(waveform1,waveform2,t1,t2)
+vidage()
+
+#On demande a l'utilisateur s'il veut enregistrer son fichier
+sav = input("Désirez vous sauvegarder les données ? (y/n)")
+if sav == "y" :
+    try :
+        fichier = input("Indiquer le nom du fichier : ")
+        sauvegarde(fichier,t,waveform1,waveform2)
+    except :
+        print ("Une erreur est survenue, enregistrement impossible")
 
 
